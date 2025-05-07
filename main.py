0d2f5787e17c1617b50ebe045d944bdc948a0c3f
chloeepark <silverwestpark@gmail.com>
2025-05-07 19:15:30 +0900

    update main

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import nltk
+from nltk.data import find
 from app import create_app
+
+def ensure_nltk_data():
+    try:
+        find('tokenizers/punkt')
+    except LookupError:
+        nltk.download('punkt')
+
+    try:
+        find('sentiment/vader_lexicon')
+    except LookupError:
+        nltk.download('vader_lexicon')
+
+ensure_nltk_data()
 
 app = create_app()
 
